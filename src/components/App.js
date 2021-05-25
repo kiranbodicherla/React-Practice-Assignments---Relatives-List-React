fa0c0af91f40b94e854d077641575b265768d1f0
kiranbodicherla <kiranbits199@gmail.com>
2021-05-25 15:39:43 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,15 @@ import '../styles/App.css';
 
 class App extends Component {
     render() {
+        const relatives = [{id:"relativeListItem1",name:"kiran"},{id:"relativeListItem2",name:"prem"},{id:"relativeListItem3",name:"shiva"},{id:"relativeListItem4",name:"rohit"},{id:"relativeListItem5",name:"akhil"}
+    ];
+    const listItem = relatives.map((names)=>
+    <li key={names.id}>{names.name}</li>);
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+                <ol key={listItem.key}>{listItem}</ol>
+              
             </div>
         )
     }
